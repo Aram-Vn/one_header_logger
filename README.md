7e69cb143dd7658e512da100668bccb45b24d3cc
Aram-Vn <aramvardanyan13@gmail.com>
2025-06-13 14:34:17 +0400

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # one_header_logger
+
+log levels setup
+
+```cpp
+#define LOG_LEVEL_THRESHOLD LOG_LEVEL_DEBUG
+#define LOG_LEVEL_THRESHOLD LOG_LEVEL_INFO
+#define LOG_LEVEL_THRESHOLD LOG_LEVEL_WARN
+#define LOG_LEVEL_THRESHOLD LOG_LEVEL_ERROR
+```
+
+or give flag
+
+```sh
+-DLOG_LEVEL_THRESHOLD=LOG_LEVEL_DEBUG
+-DLOG_LEVEL_THRESHOLD=LOG_LEVEL_INFO
+-DLOG_LEVEL_THRESHOLD=LOG_LEVEL_WARN
+-DLOG_LEVEL_THRESHOLD=LOG_LEVEL_ERROR
+
+```
+
+```cmake
+add_compile_definitions(LOG_LEVEL_DEBUG)
+add_compile_definitions(LOG_LEVEL_INFO)
+add_compile_definitions(LOG_LEVEL_WARN)
+add_compile_definitions(LOG_LEVEL_ERROR)
+```
+
+for mutex enable
+
+```sh
+-DENABLE_LOG_MUTEX
+```
+
+```cmake
+add_compile_definitions(ENABLE_LOG_MUTEX)
+```
+
+```cpp
+#define ENABLE_LOG_MUTEX
+```
+
+```sh
+-DENABLE_LOG_MUTEX
+```
--- logger.hpp
+++ logger.hpp
@@ -19,13 +19,14 @@
 
 #undef log
 
-// Optional thread safety
 #ifdef ENABLE_LOG_MUTEX
 #include <mutex>
 inline std::mutex log_mutex;
-#define LOCK_LOG() std::lock_guard<std::mutex> lock(log_mutex)
+#define LOCK_LOG()           std::lock_guard<std::mutex> lock(log_mutex)
+#define IS_LOG_MUTEX_ENABLED 1
 #else
-#define LOCK_LOG() (void)0
+#define LOCK_LOG()           (void)0
+#define IS_LOG_MUTEX_ENABLED 0
 #endif
 
 #if defined(__GNUC__) && !defined(__clang__)
@@ -33,51 +34,63 @@ inline std::mutex log_mutex;
 #define log(level, fmt, ...)                                                                                           \
     do                                                                                                                 \
     {                                                                                                                  \
-        if ((level) >= LOG_LEVEL_THRESHOLD)                                                                            \
+        LOCK_LOG();                                                                                                    \
+        const char* lvl_str = (level == LOG_LEVEL_ERROR)  ? "[" RED "ERROR" RESET "]"                                  \
+                              : (level == LOG_LEVEL_WARN) ? "[" YELLOW "WARN" RESET "]"                                \
+                              : (level == LOG_LEVEL_INFO) ? "[" GREEN "INFO" RESET "]"                                 \
+                                                          : "[" BLUE "DEBUG" RESET "]";                                \
+        if (level == LOG_LEVEL_INFO || level == LOG_LEVEL_DEBUG)                                                       \
         {                                                                                                              \
-            LOCK_LOG();                                                                                                \
-            const char* lvl_str = (level == LOG_LEVEL_ERROR)  ? "[" RED "ERROR" RESET "]"                              \
-                                  : (level == LOG_LEVEL_WARN) ? "[" YELLOW "WARN" RESET "]"                            \
-                                  : (level == LOG_LEVEL_INFO) ? "[" GREEN "INFO" RESET "]"                             \
-                                                              : "[" BLUE "DEBUG" RESET "]";                            \
-            if (level == LOG_LEVEL_INFO || level == LOG_LEVEL_DEBUG)                                                   \
-            {                                                                                                          \
-                fprintf(stderr, "%s ", lvl_str);                                                                       \
-            }                                                                                                          \
-            else                                                                                                       \
-            {                                                                                                          \
-                fprintf(stderr, "%s %s:%d %s(): ", lvl_str, __FILE__, __LINE__, __func__);                             \
-            }                                                                                                          \
-            fprintf(stderr, fmt, ##__VA_ARGS__);                                                                       \
+            fprintf(stderr, "%s ", lvl_str);                                                                           \
         }                                                                                                              \
+        else                                                                                                           \
+        {                                                                                                              \
+            fprintf(stderr, "%s %s:%d %s(): ", lvl_str, __FILE__, __LINE__, __func__);                                 \
+        }                                                                                                              \
+        fprintf(stderr, fmt, ##__VA_ARGS__);                                                                           \
     } while (0)
 #else
 // C++ standard-compliant version
 #define log(level, ...)                                                                                                \
     do                                                                                                                 \
     {                                                                                                                  \
-        if ((level) >= LOG_LEVEL_THRESHOLD)                                                                            \
+        LOCK_LOG();                                                                                                    \
+        const char* lvl_str = (level == LOG_LEVEL_ERROR)  ? "[" RED "ERROR" RESET "]"                                  \
+                              : (level == LOG_LEVEL_WARN) ? "[" YELLOW "WARN" RESET "]"                                \
+                              : (level == LOG_LEVEL_INFO) ? "[" GREEN "INFO" RESET "]"                                 \
+                                                          : "[" BLUE "DEBUG" RESET "]";                                \
+        if (level == LOG_LEVEL_INFO || level == LOG_LEVEL_DEBUG)                                                       \
         {                                                                                                              \
-            LOCK_LOG();                                                                                                \
-            const char* lvl_str = (level == LOG_LEVEL_ERROR)  ? "[" RED "ERROR" RESET "]"                              \
-                                  : (level == LOG_LEVEL_WARN) ? "[" YELLOW "WARN" RESET "]"                            \
-                                  : (level == LOG_LEVEL_INFO) ? "[" GREEN "INFO" RESET "]"                             \
-                                                              : "[" BLUE "DEBUG" RESET "]";                            \
-            if (level == LOG_LEVEL_INFO || level == LOG_LEVEL_DEBUG)                                                   \
-            {                                                                                                          \
-                fprintf(stderr, "%s ", lvl_str);                                                                       \
-            }                                                                                                          \
-            else                                                                                                       \
-            {                                                                                                          \
-                fprintf(stderr, "%s %s:%d %s(): ", lvl_str, __FILE__, __LINE__, __func__);                             \
-            }                                                                                                          \
-            fprintf(stderr, __VA_ARGS__);                                                                              \
+            fprintf(stderr, "%s ", lvl_str);                                                                           \
         }                                                                                                              \
+        else                                                                                                           \
+        {                                                                                                              \
+            fprintf(stderr, "%s %s:%d %s(): ", lvl_str, __FILE__, __LINE__, __func__);                                 \
+        }                                                                                                              \
+        fprintf(stderr, __VA_ARGS__);                                                                                  \
     } while (0)
 #endif
 
-// Logging functions
+#if LOG_LEVEL_DEBUG >= LOG_LEVEL_THRESHOLD
 #define log_debug(...) log(LOG_LEVEL_DEBUG, __VA_ARGS__)
-#define log_info(...)  log(LOG_LEVEL_INFO, __VA_ARGS__)
-#define log_warn(...)  log(LOG_LEVEL_WARN, __VA_ARGS__)
+#else
+#define log_debug(...) (void)0
+#endif
+
+#if LOG_LEVEL_INFO >= LOG_LEVEL_THRESHOLD
+#define log_info(...) log(LOG_LEVEL_INFO, __VA_ARGS__)
+#else
+#define log_info(...) (void)0
+#endif
+
+#if LOG_LEVEL_WARN >= LOG_LEVEL_THRESHOLD
+#define log_warn(...) log(LOG_LEVEL_WARN, __VA_ARGS__)
+#else
+#define log_warn(...) (void)0
+#endif
+
+#if LOG_LEVEL_ERROR >= LOG_LEVEL_THRESHOLD
 #define log_error(...) log(LOG_LEVEL_ERROR, __VA_ARGS__)
+#else
+#define log_error(...) (void)0
+#endif
